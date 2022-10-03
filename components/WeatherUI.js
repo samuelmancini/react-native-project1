e019c41f26c7d9e024a5c3b6783a5ac9d46817f2
Samuel <samuelmancini13@gmail.com>
2022-10-03 20:28:51 +0200

    added card via react-native-paper with icon

--- components/WeatherUI.js
+++ components/WeatherUI.js
@@ -1,25 +1,35 @@
 import {useState} from "react";
 import {StyleSheet, Text, View} from "react-native";
+import {Card, Paragraph, Title} from "react-native-paper";
 
 export default function WeatherUI(props) {
+    let pictureUrls = [
+        {}
+    ]
     return (
-        <View style={props.mode ? styles.container2 : styles.container}>
-            <Text style={props.mode ? styles.white : styles.black}>PLZ: {props.plz.name}</Text>
-            <Text style = {props.mode ? styles.white : styles.black}>Weather: {props.plz.weather[0].description}</Text>
-            <Text style = {props.mode ? styles.white : styles.black}>Temperature: {Math.floor(props.plz.main.temp - 273.15)}°</Text>
+        <View>
+            <Card style={props.mode ? styles.container2 : styles.container}>
+                <Card.Title/>
+                <Card.Content style = {styles.cardContent}>
+                    <Title style={props.mode ? styles.white : styles.black}>{props.plz.name}</Title>
+                    <Paragraph style={props.mode ? styles.white : styles.black}>{props.plz.weather[0].description}</Paragraph>
+                    <Paragraph style={props.mode ? styles.white : styles.black}>{Math.floor(props.plz.main.temp - 273.15)}°</Paragraph>
+                </Card.Content>
+                <Card.Cover source={{uri: 'https://openweathermap.org/img/w/' + props.plz.weather[0].icon + '.png'}}/>
+            </Card>
         </View>
     )
 };
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: '#fff',
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
     },
     container2: {
-        flex: 1,
         backgroundColor: '#000',
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
     },
@@ -28,5 +38,11 @@ const styles = StyleSheet.create({
     },
     black: {
         color: '#000',
-    }
+    },
+    cardContent: {
+        flex: 1,
+        borderRadius: 100,
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
